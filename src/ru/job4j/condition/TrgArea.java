698d914d11b87c9541197243284cea90fda54d9c
Aleksey Zakharov <flex021981@gmail.com>
2020-09-21 15:23:27 +0300

    CheckStyle

--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -10,6 +10,6 @@ public class TrgArea {
     public static void main(String[] args) {
         double rsl = TrgArea.area(2, 2, 2);
         System.out.println("area (2, 2, 2) = " + rsl);
-        System.out.println("area (6, 6, 6) = " + TrgArea.area(6,6,6));
+        System.out.println("area (6, 6, 6) = " + TrgArea.area(6, 6, 6));
     }
 }
