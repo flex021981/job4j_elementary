3156f83040d2f1c4dcf4a2d689f17c53c16ab053
Aleksey Zakharov <flex021981@gmail.com>
2020-09-21 10:59:15 +0300

    3.3. Разбор первой программы. [#369228] Create file Multiple.java

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(1*i);
+        }
+        System.out.println();
+        System.out.println(1*1);
+        System.out.println(1*2);
+        System.out.println(1*3);
+        System.out.println(1*4);
+        System.out.println(1*5);
+        System.out.println(1*6);
+        System.out.println(1*7);
+        System.out.println(1*8);
+        System.out.println(1*9);
+        System.out.println(1*10);
+    }
+}
