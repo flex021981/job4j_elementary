b54ac556d7f6fc53460d70e4a0272727800f5534
Aleksey Zakharov <flex021981@gmail.com>
2020-09-22 10:29:50 +0300

    2. Тесты для задачи "идеальный вес" [#369242]

--- src/ru/job4j/calculator/FitTest.java
+++ src/ru/job4j/calculator/FitTest.java
@@ -3,6 +3,9 @@ package ru.job4j.calculator;
 import org.junit.Assert;
 import org.junit.Test;
 
+
+import static org.hamcrest.core.Is.is;
+
 public class FitTest {
 
     @Test
@@ -14,9 +17,15 @@ public class FitTest {
     }
 
     @Test
-    public void whenMan170Then89() {
+    public void whenMan170() {
         short in = 170;
-        double expected = 89;
-        double out = Fit.womanWeight(in);
+        double expected = 80.5;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenMan170Then89() {
+        Assert.assertThat(Fit.manWeight((short) 170), is(89));
     }
 }
