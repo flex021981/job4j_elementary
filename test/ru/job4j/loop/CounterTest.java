2872e829094cb308cf789f4089dc4dc085e2b6c0
Aleksey Zakharov <flex021981@gmail.com>
2020-09-23 09:05:07 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#369273]

---
+++ test/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int rsl = Counter.sumByEven(1, 10);
+        int expected = 30;
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenSumEvenNumbersFrom3To8() {
+        assertThat(Counter.sumByEven(3, 8), is(18));
+    }
+}
