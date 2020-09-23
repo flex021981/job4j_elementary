a27d2a8a349ce3134b2610ca38104d7ab4f2e21b
Aleksey Zakharov <flex021981@gmail.com>
2020-09-23 12:20:42 +0300

    5.5. Простое число [#369268] - Исправил ошибки

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,13 +2,10 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        if (number == 1) {
-            return false;
-        }
+        boolean prime = number > 1;
         for (int i = 2; i < number; i++) {
-            prime = !(number % i == 0);
-            if (!prime) {
+            if (number % i == 0) {
+                prime = false;
                 break;
             }
         }
