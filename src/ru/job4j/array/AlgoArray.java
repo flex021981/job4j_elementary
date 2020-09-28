9dc78d83786fa383ca23d489a5fced74fdfdab58
Aleksey Zakharov <flex021981@gmail.com>
2020-09-28 10:30:05 +0300

    fix code stile

--- src/ru/job4j/array/AlgoArray.java
+++ src/ru/job4j/array/AlgoArray.java
@@ -15,7 +15,6 @@ public class AlgoArray {
         array[3] = array[4];
         array[4] = temp;
 
-
         for (int index = 0; index < array.length; index++) {
             System.out.println(array[index]);
         }
